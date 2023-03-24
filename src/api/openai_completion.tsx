3f37f6415620721076309b3f68ec461fce3fe35e
pgzmnk <pgzmnk@gmail.com>
2023-03-24 10:24:58 -0600

    Clean-up inference.

--- src/api/openai_completion.tsx
+++ src/api/openai_completion.tsx
@@ -1,5 +1,7 @@
-// function calls openai completion method with a prompt and returns the response
-export default async function openai_completion(prompt: string): Promise<string> {
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Configuration, OpenAIApi } from 'openai';
+
+export async function OpenaiCompletion(prompt: string): Promise<string> {
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     });
@@ -10,5 +12,5 @@ export default async function openai_completion(prompt: string): Promise<string>
         max_tokens: 7,
         temperature: 0,
     });
-    return response.data.choices[0].text;
+    return response.data.choices[0].text as string;
 }
--- src/components/Inference.tsx
+++ src/components/Inference.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { Configuration, OpenAIApi } from 'openai';
 
 export const Inference: React.FC<any> = () => {
     React.useEffect(() => {
         async function fetchData() {
             console.log('fetching data');
-            console.log(process.env.OPENAI_API_KEY);
-            const configuration = new Configuration({
-                apiKey: process.env.OPENAI_API_KEY,
-            });
-            // const openai = new OpenAIApi(configuration);
-            // const response = await openai.createCompletion({
-            //     model: 'text-davinci-003',
-            //     prompt: 'Say this is a test',
-            //     max_tokens: 7,
-            //     temperature: 0,
-            // });
-            console.log('placeholder openai response');
-            // console.log(response);
-            // console.log(response.data.choices[0].text);
         }
         fetchData();
-    }, []); // Or [] if effect doesn't need props or state
+    }, []);
 
     return (
         <div className="">
